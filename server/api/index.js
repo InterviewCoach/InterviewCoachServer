f588d62a618f3f6f9ad4c22f4cb0bc833eff96d8
Malka <Malka.in.red@gmail.com>
2019-11-26 17:17:10 -0500

    feat: add speech route

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,8 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/sessions', require('./sessions'))
 router.use('/questions', require('./questions'))
+router.use('/speech', require('./speech'))
+
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
