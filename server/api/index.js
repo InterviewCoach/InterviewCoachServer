048ca7826fbe4261abbbdfb3f6d3317d1c2ee747
malkamoskowicz <47335288+malkamoskowicz@users.noreply.github.com>
2019-11-27 18:19:22 -0500

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,8 @@ router.use('/users', require('./users'))
 router.use('/sessions', require('./sessions'))
 router.use('/questions', require('./questions'))
 router.use('/speech', require('./speech'))
+router.use('/speech2', require('./speech2'))
+
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
