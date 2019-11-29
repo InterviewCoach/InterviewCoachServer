bb50a6c33d29f5bc4a60869c99becc94a3f2355c
GNz11 <sshammasova@gmail.com>
2019-11-29 00:21:31 -0500

    try another encoding

--- server/api/speech2.js
+++ server/api/speech2.js
@@ -22,8 +22,8 @@ router.post('/', async (req, res, next) => {
     }
 
     const config = {
-      encoding: 'AMR_WB',
-      sampleRateHertz: 16000,
+      encoding: 'AMR',
+      sampleRateHertz: 8000,
       languageCode: 'en-US'
     }
 
