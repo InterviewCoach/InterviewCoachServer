5d64265a6c9deac845b1a5150f407dbbf3971738
Malka <Malka.in.red@gmail.com>
2019-12-01 21:23:29 -0500

    add post route for toxicity

--- server/api/speech2.js
+++ server/api/speech2.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const speech = require('@google-cloud/speech')
-// const toxicity = require('@tensorflow-models/toxicity')
+const toxicity = require('@tensorflow-models/toxicity')
 
 const router = require('express').Router()
 module.exports = router
@@ -32,25 +32,46 @@ router.post('/', async (req, res, next) => {
 
     // Detects speech in the audio file
     const [response] = await client.recognize(request)
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
+    const transcription = response.results.map(
+      result => result.alternatives[0].transcript
+    )
     console.log('Transcription: ', transcription)
-    //get toxicity labels
-    //const model = await toxicity.load(0.9)
-    //const predictions = await model.classify(transcription)
-
-    //loop through all labels, filter out the ones that are a match
-//     const toxicity = predictions
-//       .filter(prediction => {
-//         return prediction.results[0].match
-//       })
-//       .map(prediction => prediction.label)
-
-//     res.send({transcription, toxicity})
     res.json(transcription)
     // res.send(file)
   } catch (error) {
     console.error('error: ', error)
+    res.status(500).send(error)
+  }
+})
+
+router.post('/toxicity', async (req, res, next) => {
+  try {
+    //grab data and put it in correct format for model
+    const data = req.body.data
+    const transcription = []
+    data.forEach(string => {
+      const array = string.split(' ')
+      array.forEach(word => transcription.push(word))
+    })
+
+    //if there is no transcript, just return
+    if (!transcription || !transcription.length) res.json([])
+    else {
+      //get toxicity labels
+      const model = await toxicity.load(0.9)
+      const predictions = await model.classify(transcription)
+
+      // loop through all labels, filter out the ones that are a match
+      const labels = predictions
+        .filter(prediction => {
+          return prediction.results[0].match || prediction.results[1].match
+        })
+        .map(prediction => prediction.label)
+
+      // return labels of toxicity
+      res.json(labels)
+    }
+  } catch (error) {
     res.status(500).send(error)
   }
 })
